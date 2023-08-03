85df912efd70b9b7b594b3bd9aacadb50edf16c5
Nastasya <nastasya@skiff.com>
2023-08-03 18:27:12 +0200

    Implemented dynamic content change and style enhancements for chip components

--- scripts/main.js
+++ scripts/main.js
@@ -34,7 +34,9 @@ const data = {
     title: "Strategic Planning",
     text: "Define goals and initiatives, visualize priorities and outcomes, and watch those plans pay off in a big way. It's easier when you never run out of space.   ",
     image: "./images/strategic.png"
-  }
+  }, 
+ 
+
 };
 
 // Function to change the content
@@ -57,3 +59,100 @@ document.querySelectorAll('.chip').forEach(chip => {
     changeContent(e);
   });
 });
+
+
+
+(function() {
+  const data = {
+      "UX & Design": {
+        points: [
+          "Build low-fi wireframes",
+          "Involve stakeholders in the design process",
+          "Run engaging design workshops"
+        ],
+        image: "./images/UXDesign.png"
+      },
+      "Marketing": {
+        points: [
+          "Brainstorm marketing strategies",
+          "Map your campaign in one place",
+          "Run kick-off meetings"
+        ],
+        image: "./images/marketing.png"
+      },
+      "Product Management": {
+        points: [
+          "Create product roadmaps",
+          "Discover customer needs with journey maps",
+          "Build wireframes and user flows"
+        ],
+        image: "./images/scrum.png"
+      },
+      "Engineering": {
+        points: [
+          "Run efficient estimation sessions",
+          "Diagram complex architecture",
+          "Organize hybrid scrum events"
+        ],
+        image: "./images/Engineering.png"
+      },
+      "Consultants": {
+        points: [
+          "Engage clients in interactive meetings",
+          "Solve problems faster in one virtual hub ",
+          "Leverage made-for-you facilitation features, like Miro Smart Meetings"
+        ],
+        image: "./images/Consultants.png"
+      },
+      "Agile Coaches": {
+        points: [
+          "Run collaborative scrum events",
+          "Bring teams to one vision with PI Planning",
+          "Easily integrate task tracking with Jira cards"
+        ],
+        image: "./images/agile.png"
+      },
+      "Sales": {
+        points: [
+          "Run engaging meetings to wow clients",
+          "Map out accounts to see the big picture",
+          "Plot key workflows with process diagramming"
+        ],
+        image: "./images/sales.png"
+      },
+      
+    };
+    
+    // Function to change the content
+    function changeContent(e) {
+      const chipName = e.target.textContent;
+      const chipData = data[chipName];
+    
+      if(chipData) {
+        // Clear current points
+        const pointsList = document.querySelector('.teams__points ul');
+        while (pointsList.firstChild) {
+          pointsList.firstChild.remove();
+        }
+    
+        // Add new points
+        chipData.points.forEach(point => {
+          const li = document.createElement('li');
+          li.innerHTML = `<img src="./images/points.svg" alt="">${point}`;
+          pointsList.appendChild(li);
+        });
+    
+        // Change the image
+        document.querySelector('.teams__img').src = chipData.image;
+      }
+    }
+    
+    // Adding event listeners to the chips
+    document.querySelectorAll('.chip').forEach(chip => {
+      chip.addEventListener('click', function(e) {
+        document.querySelector('.chip__active').classList.remove('chip__active');
+        e.target.classList.add('chip__active');
+        changeContent(e);
+      });
+    });
+})();
